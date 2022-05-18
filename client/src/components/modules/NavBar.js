c2256ba5cefb14073c1a2c7f8b9c157dd1664feb
Rpan4853 <raymondpan12345@gmail.com>
2022-05-17 23:00:13 -0400

    Implemented login

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { UserContext } from "../../UserContext";
 import "./NavBar.css";
 import { Navbar, Container, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const NavBar = () => {
+  const { email, verified, handleLogin, handleLogout } =
+    useContext(UserContext);
+
   return (
     <Navbar bg="light" sticky="top">
       <Container>
@@ -14,7 +18,16 @@ const NavBar = () => {
             alt="logo"
           />
         </Navbar.Brand>
-        <Button variant="primary">Log In</Button>
+        <Navbar.Text>{email}</Navbar.Text>
+        {!verified ? (
+          <Button variant="primary" onClick={handleLogin}>
+            Log In
+          </Button>
+        ) : (
+          <Button variant="danger" onClick={handleLogout}>
+            Log Out
+          </Button>
+        )}
       </Container>
     </Navbar>
   );
